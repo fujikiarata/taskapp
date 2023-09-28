6ac8bc848a743a3e1705ccdbf0e1f314ab2e7f9a
a.fujiki <a.fujiki@alphapolis.co.jp>
2023-09-28 22:17:13 +0900

    category登録

---
+++ taskapp/task.swift
@@ -0,0 +1,19 @@
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @Persisted(primaryKey: true) var id: ObjectId
+
+    // タイトル
+    @Persisted var title = ""
+    
+    // カテゴリー
+    @Persisted var category = ""
+
+    // 内容
+    @Persisted var contents = ""
+
+    // 日時
+    @Persisted var date = Date()
+}
